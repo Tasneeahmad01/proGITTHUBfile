a4e3ad7cf461f93a09786c9cf2e1f90c24f8befe
Your Name <you@example.com>
2024-02-26 14:38:23 +0530

    add

---
+++ index2.js
@@ -0,0 +1,15 @@
+function generateOTP(length){
+    let otp = '';
+    for(let i= 0;i< length;i++){
+    otp += Math.floor(Math.random()*10).toString();
+    }
+    return otp;
+}
+console.log(generateOTP(4));
+let a = 32;
+let b = 65;
+let sum =  (" sum is : " ,a+b);
+let c = 32;
+let d = 65;
+console.log(c++);
+console.log(++d);
